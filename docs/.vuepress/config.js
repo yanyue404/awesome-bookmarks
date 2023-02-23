918e099f169c392bb86b4097eba99f91443c38fa
RainBow <xiaoyueyue165@gmail.com>
2023-02-24 00:04:09 +0800

    docs: 添加链接

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,15 +20,27 @@ module.exports = {
       {
         text: "文章",
         link: "/article/"
+      },
+      {
+        text: "个人博客",
+        link: "https://yanyue404.github.io/blog/#/"
+      },
+      {
+        text: "前端知识体系",
+        link: "https://yanyue404.github.io/fe-attitude"
+      },
+      {
+        text: "王明道文集精选",
+        link: "https://yanyue404.github.io/mingdao/"
       }
     ],
     sidebar: utils.inferSiderbars(),
     sidebarDepth: 3,
     displayAllHeaders: true,
-    repo: "yanyue404/my-bookmarks",
     editLinks: true,
     docsDir: "docs",
     editLinkText: "在 GitHub 上编辑此页",
+    repo: "yanyue404/my-bookmarks",
     lastUpdated: "上次更新"
   }
 };
